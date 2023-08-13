bc87840c203568be71ea3193849889e12ea378b9
Anna Kors <ankors320@gmail.com>
2023-08-13 17:55:22 +0300

    Seminar3

---
+++ HW3/Sem/Main.java
@@ -0,0 +1,19 @@
+package HW3.Sem;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LinkedList<String> list = new LinkedList<>();
+
+        list.addSorted("Hello");
+        list.addSorted("GB");
+        list.addSorted("geekBrains");
+        list.addSorted("GeekBrains");
+        list.addSorted("World");
+        list.addSorted("hello");
+        list.addSorted("11");
+        list.addSorted("100");
+
+        list.print();
+    }
+}
